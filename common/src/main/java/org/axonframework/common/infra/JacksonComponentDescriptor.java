3b8a723bffb93e72c096d8185d96975fa6dc1a12
Steven van Beelen <steven.vanbeelen@axoniq.io>
2026-05-13 08:33:30 +0200

    Apply suggestion from @smcvb

--- common/src/main/java/org/axonframework/common/infra/JacksonComponentDescriptor.java
+++ common/src/main/java/org/axonframework/common/infra/JacksonComponentDescriptor.java
@@ -196,7 +196,7 @@ public class JacksonComponentDescriptor implements ComponentDescriptor {
     /**
      * Provides a description of what has been described in this Descriptor as a String.
      *
-     * @return A description of what has been described in this Descriptor.
+     * @return a description of what has been described in this Descriptor
      */
     public String describe() {
         try {
